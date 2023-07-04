d8e7b6b752423ce111e295ebd13bdffdd3a18a0c
Anatoliy Petrov <ripvantesla@gmail.com>
2023-07-04 17:14:21 +0600

    -added device info for tests report

--- confHelper/configHelperAndroid/configHelperAndroid.py
+++ confHelper/configHelperAndroid/configHelperAndroid.py
@@ -1,8 +1,10 @@
-import pytest
+import json
 
+import pytest
 from appium import webdriver
 from helpers import take_screenshot_and_logcat
 from confHelper.configHelperAndroid.devices_capabilities import device
+import allure
 
 APPIUM_LOCAL_HOST_URL = 'http://localhost:4723/wd/hub'
 
@@ -10,14 +12,13 @@ APPIUM_LOCAL_HOST_URL = 'http://localhost:4723/wd/hub'
 class capsAnroid:
     @pytest.fixture(scope="function")
     def driverAndroid(self, request, device_logger):
-
-        # Для смены девайса нужно сменить ключь в device.get(1)
-        # Доступные ключи Pixel 3A XL == 1 ,  Xiaomi Note 5  ==  2 , Samsung S10 == 3 ,
-        # Homtom HT17 == 4 , Sony Z2 == 5, Samsung S10 == 6 (Исключительно для ночных работ)
-
         calling_request = request._pyfuncitem.name
+        device_info = device.get(1)
 
-        driver = webdriver.Remote(APPIUM_LOCAL_HOST_URL, device.get(1))
+        with allure.step("Step #0 Device Information"):
+            allure.attach(json.dumps(device_info, indent=4), "Device Info", allure.attachment_type.JSON)
+
+        driver = webdriver.Remote(APPIUM_LOCAL_HOST_URL, device_info)
 
         def fin():
             take_screenshot_and_logcat(driver, device_logger, calling_request)
